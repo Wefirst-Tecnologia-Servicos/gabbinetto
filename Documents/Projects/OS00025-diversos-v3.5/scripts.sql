f540645d2872f5c55a300090f1fcd00c3d092d37
victorprospero@hotmail.com <victorprospero@hotmail.com>
2022-04-12 16:25:19 -0300

    bugfix sobre a versão 3.5.1

--- Documents/Projects/OS00025-diversos-v3.5/scripts.sql
+++ Documents/Projects/OS00025-diversos-v3.5/scripts.sql
@@ -4,8 +4,8 @@ USE averp;
 
 ALTER TABLE av_avaliador ADD COLUMN ativo TINYINT NOT NULL DEFAULT 1;
 
---> NOT SENT:
-
 ALTER TABLE av_avaliador ADD COLUMN notas_rh TINYINT NOT NULL DEFAULT 0;
 UPDATE av_avaliador SET notas_rh = 1 WHERE id_usuario IN (130, 155, 140, 184);
 COMMIT;
+
+--> NOT SENT:
